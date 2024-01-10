e047161e9238bbc2041e877a46b54a061bbb7ec6
Igor <zennodroid_bot1@mail.ru>
2024-01-10 21:40:17 +0600

    Technical specification "Chat server"

---
+++ ChatServer.java
@@ -0,0 +1,36 @@
+package ChatServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ChatServer {
+    ServerSocket serverSocket;
+    ArrayList<Client> clients = new ArrayList<>();
+
+    ChatServer() throws IOException {
+        serverSocket = new ServerSocket(1234);
+
+    }
+
+    public void sendAll(String message) {
+        for (Client client : clients) {
+            client.receive(message);
+        }
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println("Waiting...");
+
+            try {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected!");
+                clients.add(new Client(socket, this));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+package ChatServer;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] strings) throws IOException {
+        new ChatServer().run();
+    }
+}
