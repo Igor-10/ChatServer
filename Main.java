e8d52382c73e61a60dd0f45f9a058aa06a4aef05
Igor <zennodroid_bot1@mail.ru>
2024-01-10 22:07:57 +0600

    Technical specification "Chat server"

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package ChatServer;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] strings) throws IOException {
+    public static void main(String[] arg) throws IOException {
         new ChatServer().run();
     }
 }
